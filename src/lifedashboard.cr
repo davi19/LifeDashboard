601b50e5374c818c8e8b6a1134b34ddb54cab7a8
davi19 <davi19_19@hotmail.com>
2026-06-18 17:57:02 -0300

    login screen

--- src/lifedashboard.cr
+++ src/lifedashboard.cr
@@ -1,11 +1,18 @@
 # TODO: Write documentation for `Lifedashboard`
 require "kemal"
+public_folder "public"
 
 module Lifedashboard
   VERSION = "0.1.0"
   get "/" do
-      "Hello World"
+    render "src/views/login.ecr", "src/views/layouts/layout.ecr"
   end
 
-Kemal.run
+
+
+  post "/" do |env|
+
+   puts       env.params.body
+  end
+    Kemal.run
 end
